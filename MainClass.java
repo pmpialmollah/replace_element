8e5f56e8aeb79091e061dbf0a4fbc04b8bde5cd3
PIAL MOLLAH <pial.mollah.0401@gmail.com>
2025-12-15 12:55:41 +0600

    Problem solved v2

--- MainClass.java
+++ MainClass.java
@@ -10,16 +10,14 @@ class MainClass {
 
     }
     public static int[] replaceElement(int[] array){
-        for(int i=0; i<array.length-1; i++){
-            int maxNum = array[i+1];
-            for(int j=i+1; j<array.length; j++){
-                if (array[j] > maxNum) {
-                    maxNum = array[j];
-                }
-            }
+        int maxNum = -1;
+        for(int i= array.length - 1; i>=0; i--){
+            int temp = array[i];
             array[i] = maxNum;
+            if (temp>maxNum) {
+                maxNum = temp;
+            }
         }
-        array[array.length-1] = - 1;
         return array;
     }
     
